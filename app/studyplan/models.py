b5557cf86683448a49839b6e3be124cc790ff79c
devJosePaz <lusjos40@gmail.com>
2025-08-11 14:27:11 -0300

    feat(models): adição da tabela 'study_plans' utilizando o SQLAlchemy

---
+++ app/studyplan/models.py
@@ -0,0 +1,16 @@
+import uuid
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, mapped_column
+from app.database import Base
+
+class StudyPlan(Base):
+    __tablename__ = "study_plans"
+
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), index=True, primary_key=True, default=uuid.uuid4)
+    theme: Mapped[str] = mapped_column(String(80), nullable=False)
+    daily_time: Mapped[int] = mapped_column(Integer, nullable=False)
+    objective: Mapped[str] = mapped_column(String(80), nullable=False)
+    learning_style: Mapped[str] = mapped_column(String(80), nullable=False)
+    
+
