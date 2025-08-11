295b247a6d5e4e95e56e9ccb44dbe3a88ea6ff99
devJosePaz <lusjos40@gmail.com>
2025-08-11 14:28:22 -0300

    feat(schemas): adição dos schemas de criação e respostas para os endpoints de plano de estudos utilizando o pydantic.

---
+++ app/studyplan/schemas.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+from uuid import UUID
+
+class StudyPlanCreate(BaseModel):
+    theme: str
+    daily_time: int
+    objective: str
+    learning_style: str
+
+class StudyPlanResponse(StudyPlanCreate):
+    id: UUID
+    owner_id: UUID
+
+    class Config:
+        from_attributes = True
+
+
+
