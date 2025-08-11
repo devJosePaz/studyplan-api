d1590eeecb7d010ff3ad76e5a86577c8c9329a08
devJosePaz <lusjos40@gmail.com>
2025-08-11 14:25:39 -0300

    feat(database): adição de configurações assincronas de conexão com o banco de dados com SQLAlchemy

---
+++ app/config/database.py
@@ -0,0 +1,21 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from app.config.settings import settings
+from sqlalchemy.orm import declarative_base
+
+DATABASE_URL = settings.POSTGRES_DB
+
+engine = create_async_engine(DATABASE_URL, echo=False)
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+    autoflush=False,
+    autocommit=False
+)
+
+Base = declarative_base()
+
+async def get_async_session() -> AsyncSession:
+    async with AsyncSessionLocal() as session:
+        yield session
