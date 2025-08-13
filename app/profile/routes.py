dda5a714dc9ce59a55a27918c7065e8cfadcd581
devJosePaz <lusjos40@gmail.com>
2025-08-13 12:59:40 -0300

    feat(routes): adição da rota 'POST' de 'profile'.

---
+++ app/profile/routes.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from app.config.database import get_async_session
+from app.profile.models import ProfileModel
+from app.profile import schemas
+from typing import List
+
+router = APIRouter(prefix="/profiles", tags=["Profiles"])
+
+
+@router.post("/", response_model=schemas.ProfileResponse, status_code=status.HTTP_201_CREATED)
+async def create_profile(
+    profile_data: schemas.ProfileCreate,
+    db: AsyncSession = Depends(get_async_session)
+):
+    new_profile = ProfileModel(
+        study_method=profile_data.study_method,
+        study_daily_time=profile_data.study_daily_time,
+        study_frequence=profile_data.study_frequence,
+        main_motivation=profile_data.main_motivation
+    )
+
+    db.add(new_profile)
+    await db.commit()
+    await db.refresh(new_profile)
+
+    return new_profile
+
+
