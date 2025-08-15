dcb769421ab39cfe93aac1ba34675ab71d3701bb
devJosePaz <lusjos40@gmail.com>
2025-08-15 10:19:49 -0300

    feat(models): adição da 'class UserModel' para criar a tabela de usuários utilizando o sqlalchemy.

---
+++ app/auth/models.py
@@ -0,0 +1,18 @@
+from sqlalchemy import String, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import UUID
+from app.config.database import Base
+import uuid
+import datetime
+
+
+class UserModel(Base):
+    __tablename__ = "users"
+
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), index=True, primary_key=True)
+    email: Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
+    hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
+
+    create_at: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=datetime.datetime.utcnow)
+    update_at: Mapped[datetime]= mapped_column(DateTime, nullable=False, default=datetime.datetime, onupdate=datetime.datetime.utcnow)
+    
