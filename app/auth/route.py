d1942e3759b688caf5dc4ba57afeb6a5b6862a2b
devJosePaz <lusjos40@gmail.com>
2025-08-16 00:46:52 -0300

    feat(routes): adição do endpoint POST 'register' para realizar o cadastro do usuário.

--- app/auth/route.py
+++ app/auth/route.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from app.auth.models import UserModel
-from app.auth import schemas
+from app.auth import schemas, passw_utils
 from app.config.database import get_async_session
 from typing import List
 
@@ -14,12 +14,28 @@ async def register(
     user_data: schemas.UserCreate,
     db: AsyncSession = Depends(get_async_session)
 ):
-    pass
+    result = await db.execute(select(UserModel).where(UserModel.email == user_data.email))
+    user = result.scalars().first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="error: user already exists.")
+    
+    new_user = UserModel(
+        name=user_data.name,
+        email=user_data.email,
+        hashed_password=passw_utils.verify_password(user_data.hashed_password)
+    )
+
+    db.add(new_user)
+    await db.commit()
+    await db.refresh(new_user)
+
+    return new_user
 
 @router.get("/users", response_model=List[schemas.UserResponse], status_code=status.HTTP_200_OK)
 async def list_users(db: AsyncSession = Depends(get_async_session)):
     result = await db.execute(select(UserModel))
-    users = result.scalars.all()
+    users = result.scalars().all()
 
     if not users:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="error: users not found.")
