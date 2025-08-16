efb3466d9739ff11eeea4609059187c0dd656091
devJosePaz <lusjos40@gmail.com>
2025-08-15 23:32:24 -0300

    feat(models): adição do atributo 'user_id' e do relacionamento com o user.

--- app/studyplan/models.py
+++ app/studyplan/models.py
@@ -1,16 +1,23 @@
-import uuid
+import uuid, datetime
 from sqlalchemy import String, ForeignKey, Integer
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.config.database import Base
 
 class StudyPlanModel(Base):
     __tablename__ = "study_plans"
 
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), index=True, primary_key=True, default=uuid.uuid4)
+    user_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
     theme: Mapped[str] = mapped_column(String(80), nullable=False)
     daily_time: Mapped[int] = mapped_column(Integer, nullable=False)
     objective: Mapped[str] = mapped_column(String(80), nullable=False)
     learning_style: Mapped[str] = mapped_column(String(80), nullable=False)
+
+    create_at: Mapped[datetime] = mapped_column(Datetime, nullable=False, default=datetime.datetime.utcnow)
+
+    user: Mapped["UserModel"] = relationship(back_populates="study_plans")
+
+
     
 
