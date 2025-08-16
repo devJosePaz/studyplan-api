40e55179b92bddbb40f1e5cec65857684e7baca4
devJosePaz <lusjos40@gmail.com>
2025-08-15 22:50:33 -0300

    feat(models): adição do atributo 'name' e o relacionamento com a tabela 'profiles' na tabela 'users'.

--- app/auth/models.py
+++ app/auth/models.py
@@ -10,9 +10,12 @@ class UserModel(Base):
     __tablename__ = "users"
 
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), index=True, primary_key=True)
+    name: Mapped[str] = mapped_column(String(80), nullable=False)
     email: Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
     hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
 
     create_at: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=datetime.datetime.utcnow)
     update_at: Mapped[datetime]= mapped_column(DateTime, nullable=False, default=datetime.datetime, onupdate=datetime.datetime.utcnow)
+
+    profile: Mapped["ProfileModel"] = relationship(back_populates="user", uselist=False)
     
