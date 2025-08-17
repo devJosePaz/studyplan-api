0669b7de3998bcc5bdc9edf3c8ab02693a6b4d0e
devJosePaz <lusjos40@gmail.com>
2025-08-17 14:11:31 -0300

    fix(models): correção do atributo 'create_at' adicionando a tipagem correta do SQLAlchemy 'DateTime'.

--- app/studyplan/models.py
+++ app/studyplan/models.py
@@ -1,5 +1,5 @@
 import uuid, datetime
-from sqlalchemy import String, ForeignKey, Integer
+from sqlalchemy import String, ForeignKey, Integer, DateTime
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.config.database import Base
@@ -14,7 +14,7 @@ class StudyPlanModel(Base):
     objective: Mapped[str] = mapped_column(String(80), nullable=False)
     learning_style: Mapped[str] = mapped_column(String(80), nullable=False)
 
-    create_at: Mapped[datetime] = mapped_column(Datetime, nullable=False, default=datetime.datetime.utcnow)
+    create_at: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=datetime.datetime.utcnow)
 
     user: Mapped["UserModel"] = relationship(back_populates="study_plans")
 
