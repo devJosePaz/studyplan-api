ca69e4795b7ed68a94da61b8b729c2e7ed1380b2
devJosePaz <lusjos40@gmail.com>
2025-08-18 16:35:48 -0300

    fix(main): correção da importação da rota 'auth'.

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from app.studyplan.routes import router as study_plan_route
 from app.profile.routes import router as profile_route
+from app.auth.routes import router as auth_route
 from app.config.database import engine, Base
 
 app = FastAPI()
 
 app.include_router(study_plan_route)
 app.include_router(profile_route)
+app.include_router(auth_route)
 
 @app.on_event("startup")
 async def on_startup():
