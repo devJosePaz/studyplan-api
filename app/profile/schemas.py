fba5eed522b78699f7722712862e8f27e5b3b088
devJosePaz <lusjos40@gmail.com>
2025-08-18 21:35:17 -0300

    fix(schemas): redução de atributos para teste.

--- app/profile/schemas.py
+++ app/profile/schemas.py
@@ -4,16 +4,12 @@ from datetime import datetime
 
 class ProfileCreate(BaseModel):
     study_method: str
-    study_daily_time: int
-    study_frequence: str
     main_motivation: str
 
 
 class ProfileResponse(BaseModel):
     id: UUID
     study_method: str
-    study_daily_time: int
-    study_frequence: str
     main_motivation: str
     create_at: datetime
     update_at: datetime
