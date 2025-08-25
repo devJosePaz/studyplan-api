51160fba023f857c9da6abf04ca251ec8ae6dbec
devJosePaz <lusjos40@gmail.com>
2025-08-25 20:55:31 -0300

    refactor(schemas): alteração dos atributos dos schemas 'StudyPlanCreate' e 'StudyPlanResponse'.

--- app/studyplan/schemas.py
+++ app/studyplan/schemas.py
@@ -1,14 +1,23 @@
 from pydantic import BaseModel
 from uuid import UUID
+from datetime import datetime
 
 class StudyPlanCreate(BaseModel):
     theme: str
-    daily_time: int
+    theme_level: str
+    week_time: int
     objective: str
-    learning_style: str
 
 class StudyPlanResponse(StudyPlanCreate):
     id: UUID
+    user_id: UUID
+    theme: str
+    theme_level: str
+    week_time: int
+    objective: str
+
+    create_at: datetime
+    update_at: datetime
 
     class Config:
         from_attributes = True
