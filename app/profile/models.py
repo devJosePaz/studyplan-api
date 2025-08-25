7b5696de5a1041c1adf39772c6f9f1a4d8db1899
devJosePaz <lusjos40@gmail.com>
2025-08-25 20:35:32 -0300

    refactor(models): alteração dos componentes da tabela 'profiles'.

--- app/profile/models.py
+++ app/profile/models.py
@@ -9,8 +9,11 @@ class ProfileModel(Base):
 
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
     user_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("users.id"), unique=True, index=True)
-    study_method: Mapped[str] = mapped_column(String(50), nullable=False) # teorico, pratico, ou alguma coisa
-    main_motivation: Mapped[str] = mapped_column(String(80), nullable=False)
+    learning_style: Mapped[str] = mapped_column(String(50), nullable=False) # teorico, pratico, balanceado, intensivo
+    challenge_tolerance: Mapped[str] = mapped_column(String(50), nullable=False) # baixo, médio, alto
+    focus: Mapped[str] = mapped_column(String(50), nullable=False) # baixo, médio, alto
+    study_resilience: Mapped[str] = mapped_column(String(50), nullable=False) # baixa, média, alta
+
 
     create_at: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=datetime.datetime.utcnow)
     update_at: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
